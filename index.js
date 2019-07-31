3f30820bff2b79ddd065f78ee263705eee2fff04
Jon Caneiro <joncaneiro@gmail.com>
2019-07-31 19:42:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  
+  let arr = [];
+  
+  for (let i = 0; i < musicians.length; i++ ) {
+  arr.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  
+  return arr
+}
+
+
+
+function johnLennonFacts(facts) {
+  
+  let newFacts = [];
+  var i = 0
+  
+  while(i < facts.length) {
+    newFacts.push(`${facts[i]} + !!!`)
+  }
+  return newFacts
+}
