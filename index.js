3df496accdee4212e389f67c777c3d69f2f69704
Jon Caneiro <joncaneiro@gmail.com>
2019-07-31 19:54:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function johnLennonFacts(facts) {
   var i = 0
   
   while(i < facts.length) {
-    newFacts.push(`${facts[i]} !!!`)
+    newFacts.push(`${facts[i]}!!!`)
     i++
   }
   return newFacts
